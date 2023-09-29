b1b9dd239c98b78d71ad1b9114039aa75f6087d8
ikvankan <ikvankan@gmail.com>
2023-09-29 22:39:14 +0300

    лул

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = setTimeout(x,2700);
 function x() {
     document.body.classList.add('loaded_hiding');
+    window.scrollTo(0, 0);
     window.setTimeout(function () {
       document.body.classList.add('loaded');
       document.body.classList.remove('loaded_hiding');
