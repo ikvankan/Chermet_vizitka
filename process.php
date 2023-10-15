4b26ebb38e395150da4ec176ae3538eb585c82d1
ikvankan <ikvankan@gmail.com>
2023-10-15 22:34:06 +0300

    upgrade mail

--- process.php
+++ process.php
@@ -1,21 +1,28 @@
 <?php
-// Получение данных из POST-запроса
 $data = json_decode(file_get_contents('php://input'), true);
+$email = $data['email'];
+$name = $data['name'];
+$cart = $data['cart'];
 
-// Использование данных в функции mail()
 $message = '';
 $totalCost = 0;
-foreach ($data as $item => $details) {
+$orderNumber = rand(1, 999); // Генерируем случайный номер заказа
+
+$message .= "Имя заказчика: $name\n";
+$message .= "Номер заказа: $orderNumber\n";
+
+foreach ($cart as $item => $details) {
     $quantity = $details['quantity'];
     $price = $details['price'];
     $Name = $details['Name'];
     $itemCost = $quantity * $price;
-    $message .= "Наименование: $Name, Количетво: $quantity, Стоимость за единицу: $price, Итоговая стоимость: $itemCost\n";
+    $message .= "Наименование: $Name, Количетво: $quantity, Стоимость за единицу: {$price}р, Итоговая стоимость: {$itemCost}р\n";
     $totalCost += $itemCost;
 }
-$message .= "Итоговая стоимость всех товаров: $totalCost";
 
-$mail_sent = mail('ikvankan@gmail.com', 'Заказ', $message);
+$message .= "Итоговая стоимость всех товаров: {$totalCost}p";
+
+$mail_sent = mail("testpochtadlyavsego@gmail.com", "Заказ №$orderNumber от $name", $message);
 
 if ($mail_sent) {
     echo "Письмо успешно отправлено!";
