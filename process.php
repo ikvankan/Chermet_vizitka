a0c5085739644fcb3825581a49b4a3c8483ef783
ikvankan <ikvankan@gmail.com>
2023-10-15 22:45:47 +0300

    fix mail

--- process.php
+++ process.php
@@ -1,29 +1,38 @@
 <?php
+session_start();
 $data = json_decode(file_get_contents('php://input'), true);
 $email = $data['email'];
 $name = $data['name'];
 $cart = $data['cart'];
 
 $message = '';
+$message2 = '';
 $totalCost = 0;
-$orderNumber = rand(1, 999); // Генерируем случайный номер заказа
+if (!isset($_SESSION['orderNumber'])) {
+    $_SESSION['orderNumber'] = 0; // Инициализируем номер заказа, если он еще не установлен
+}
+
+$orderNumber = ++$_SESSION['orderNumber']; 
 
 $message .= "Имя заказчика: $name\n";
 $message .= "Номер заказа: $orderNumber\n";
+$message2 .= "Содержание заказа:\n";
 
 foreach ($cart as $item => $details) {
     $quantity = $details['quantity'];
     $price = $details['price'];
     $Name = $details['Name'];
     $itemCost = $quantity * $price;
+    $message2 .= "Наименование: $Name, Количетво: $quantity, Стоимость за единицу: {$price}р, Итоговая стоимость: {$itemCost}р\n";
     $message .= "Наименование: $Name, Количетво: $quantity, Стоимость за единицу: {$price}р, Итоговая стоимость: {$itemCost}р\n";
     $totalCost += $itemCost;
 }
 
 $message .= "Итоговая стоимость всех товаров: {$totalCost}p";
+$message2 .= "Итоговая стоимость всех товаров: {$totalCost}p";
 
 $mail_sent = mail("testpochtadlyavsego@gmail.com", "Заказ №$orderNumber от $name", $message);
-
+$mail_sent2 = mail($email, "Ваш заказ №{$orderNumber} был принят!",$message2);
 if ($mail_sent) {
     echo "Письмо успешно отправлено!";
 } else {
